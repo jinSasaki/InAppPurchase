68a7bbb8a58ff7243040c7324c8c2f1af45c6e20
Jin Sasaki <aysm47@gmail.com>
2021-08-12 11:07:57 +0900

    Add new fields for PaymentTransaction

--- Sources/PaymentTransaction.swift
+++ Sources/PaymentTransaction.swift
@@ -9,9 +9,26 @@
 import Foundation
 import StoreKit
 
-public typealias TransactionState = SKPaymentTransactionState
-
 public struct PaymentTransaction {
+    public enum State: Equatable {
+        case purchasing
+        case purchased
+        case failed
+        case restored
+        case deferred
+        case unknown(rawValue: Int)
+
+        init(_ skState: SKPaymentTransactionState) {
+            switch skState {
+            case .purchasing: self = .purchasing
+            case .purchased: self = .purchased
+            case .failed: self = .failed
+            case .deferred: self = .deferred
+            case .restored: self = .restored
+            @unknown default: self = .unknown(rawValue: skState.rawValue)
+            }
+        }
+    }
     public var transactionIdentifier: String? {
         skTransaction.transactionIdentifier
     }
@@ -20,6 +37,15 @@ public struct PaymentTransaction {
     }
     public var productIdentifier: String {
         skTransaction.payment.productIdentifier
+    }
+    public var state: State {
+        PaymentTransaction.State(skTransaction.transactionState)
+    }
+    public var original: PaymentTransaction? {
+        guard let original = skTransaction.original else {
+            return nil
+        }
+        return .init(original)
     }
 
     internal let skTransaction: SKPaymentTransaction
--- Tests/PaymentTransactionTests.swift
+++ Tests/PaymentTransactionTests.swift
@@ -9,8 +9,18 @@
 import XCTest
 @testable import InAppPurchase
 import InAppPurchaseStubs
+import StoreKit
 
 class PaymentTransactionTests: XCTestCase {
+    func testTransactionState() {
+        XCTAssertEqual(PaymentTransaction.State.purchasing, PaymentTransaction.State(.purchasing))
+        XCTAssertEqual(PaymentTransaction.State.purchased, PaymentTransaction.State(.purchased))
+        XCTAssertEqual(PaymentTransaction.State.failed, PaymentTransaction.State(.failed))
+        XCTAssertEqual(PaymentTransaction.State.restored, PaymentTransaction.State(.restored))
+        XCTAssertEqual(PaymentTransaction.State.deferred, PaymentTransaction.State(.deferred))
+        XCTAssertEqual(PaymentTransaction.State.unknown(rawValue: 10), PaymentTransaction.State(SKPaymentTransactionState(rawValue: 10) ?? .deferred))
+    }
+
     func testInit() {
         let original = StubPaymentTransaction(
             transactionIdentifier: "ORIGINAL_TRANSACTION_001",
@@ -21,14 +31,32 @@ class PaymentTransactionTests: XCTestCase {
 
         let skTransaction = StubPaymentTransaction(
             transactionIdentifier: "TRANSACTION_001",
-            transactionState: .purchased,
+            transactionState: .restored,
             original: original,
-            payment: payment)
+            payment: payment
+        )
 
         let transaction = PaymentTransaction(skTransaction)
 
         XCTAssertEqual(transaction.transactionIdentifier, "TRANSACTION_001")
         XCTAssertEqual(transaction.originalTransactionIdentifier, "ORIGINAL_TRANSACTION_001")
         XCTAssertEqual(transaction.productIdentifier, "PRODUCT_001")
+        XCTAssertEqual(transaction.state, .restored)
+        XCTAssertEqual(transaction.original?.transactionIdentifier, "ORIGINAL_TRANSACTION_001")
+        XCTAssertEqual(transaction.original?.state, .purchased)
+    }
+
+    func testInitWithoutOriginal() {
+        let payment = StubPayment(productIdentifier: "PRODUCT_001")
+
+        let skTransaction = StubPaymentTransaction(
+            transactionIdentifier: "TRANSACTION_001",
+            transactionState: .restored,
+            original: nil,
+            payment: payment
+        )
+
+        let transaction = PaymentTransaction(skTransaction)
+        XCTAssertNil(transaction.original)
     }
 }
